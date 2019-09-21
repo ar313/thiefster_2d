d168f9e43a7bcbd10feacc93b0436223a60ddde1
LordOfTheGulls <galeksanderg@gmail.com>
2019-09-21 21:49:35 +0300

    Ben idle/blink anims

---
+++ Assets/Scripts/AI/Civilian/Anim Behaviours/IdleBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleBehaviour : StateMachineBehaviour
+{
+ 
+  public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+  {
+
+
+  }
+
+  public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+  {
+    Blink(animator);
+  }
+
+  private void Blink(Animator animator)
+  {
+    if (animator.GetBool("Blink") == false)
+    {
+      int blinkChance = Random.Range(0, 3);
+
+      if (blinkChance == 2)
+      {
+        animator.SetBool("Blink", true);
+      }
+    }
+    else
+    {
+      animator.SetBool("Blink", false);
+    }
+  }
+}
